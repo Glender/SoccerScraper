bbf5636781d2eaeccdeaa0c712a29c489c4e6a32
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2022-08-01 19:44:16 +0200

    add trainer data

--- scrape_trainer_data.R
+++ scrape_trainer_data.R
@@ -1,7 +1,7 @@
 library(rvest)
 library(lubridate)
 library(tibble)
-
+library(readr)
 
 # get all trainer data from a voetbal.com url
 # assure the data is in the correct format
@@ -31,8 +31,11 @@ scrape_trainer_data <- function(url){
   # combine all data in a tibble df
   # to assure informative printing
   trainer_data <- tibble::tibble(
+    team_name = find_clubname_in_url(url),
     start_date = start_date,
     end_date = end_date,
+    start_year = lubridate::year(start_date),
+    end_year = lubridate::year(end_date),
     days_in_service = end_date - start_date,
     trainer = table_trainer_data$Trainer,
     birthyear = birth_years,
@@ -40,7 +43,19 @@ scrape_trainer_data <- function(url){
       lubridate::today() - birthyear
     ) / 365.25
   )
+  
+  cat("Scraped trainer data from:", url, "\n")
+  
   return(trainer_data)
+}
+
+# wrapper around scrape_trainer_data
+scrape_trainer_data_from_urls <- function(urls){
+  
+  data <- lapply(urls, scrape_trainer_data)
+  total_data <- do.call(rbind, data)
+  
+  return(total_data)
 }
 
 
@@ -59,3 +74,38 @@ print(data_ajax)
 # longest serving trainer for ajax?
 data_ajax %>% 
   dplyr::arrange(desc(days_in_service))
+
+
+# list with team names that need to be scaped
+team_names <- c(
+  "sc-heerenveen",
+  "fortuna-sittard",
+  "sc-cambuur",
+  "psv-eindhoven",
+  "rkc-waalwijk",
+  "fc-groningen",
+  "vitesse",
+  "nec-nijmegen",
+  "az-alkmaar",
+  "sparta-rotterdam",
+  "afc-ajax",
+  "sbv-excelsior",
+  "fc-emmen",
+  "fc-utrecht",
+  "fc-volendam",
+  "feyenoord",
+  "fc-twente",
+  "go-ahead-eagles"
+)
+
+# create url with team names
+urls <- sprintf("https://www.voetbal.com/teams/%s/9/", team_names)
+print(urls)
+
+# scrape all data from urls
+total_data <- scrape_trainer_data_from_urls(urls)
+
+print(total_data)
+
+#write_csv2(total_data, "trainer_data.csv")
+#saveRDS(total_data, "trainer_data.rds")
