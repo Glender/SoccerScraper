12f97308cf5df437da1db6106aaa4df08be4919e
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2022-08-03 21:53:58 +0200

    add quicker ld algorithm

--- levenstein_distance.cpp
+++ levenstein_distance.cpp
@@ -5,20 +5,41 @@ using namespace Rcpp;
 using namespace std;
 
 // [[Rcpp::export]]
-unsigned int edit_distance(const std::string& s1, const std::string& s2) {
+size_t uiLevenshteinDistance(const string &s1, const string &s2)
+{
+  const size_t
+  m(s1.size()),
+  n(s2.size());
   
-  // https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C++
-  const std::size_t len1 = s1.size(), len2 = s2.size();
-  std::vector<std::vector<unsigned int>> d(len1 + 1, std::vector<unsigned int>(len2 + 1));
+  if( m==0 ) return n;
+  if( n==0 ) return m;
   
-  d[0][0] = 0;
-  for(unsigned int i = 1; i <= len1; ++i) d[i][0] = i;
-  for(unsigned int i = 1; i <= len2; ++i) d[0][i] = i;
+  // allocation below is not ISO-compliant,
+  // it won't work with -pedantic-errors.
+  size_t costs[n + 1];
   
-  for(unsigned int i = 1; i <= len1; ++i)
-    for(unsigned int j = 1; j <= len2; ++j)
-      // note that std::min({arg1, arg2, arg3}) works only in C++11,
-      // for C++98 use std::min(std::min(arg1, arg2), arg3)
-      d[i][j] = std::min({ d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1) });
-  return d[len1][len2];
+  for( size_t k=0; k<=n; k++ ) costs[k] = k;
+  
+  size_t i { 0 };
+  for (char const &c1 : s1) 
+  {
+    costs[0] = i+1;
+    size_t corner { i },
+    j      { 0 };
+    for (char const &c2 : s2)
+    {
+      size_t upper { costs[j+1] };
+      if( c1 == c2 ) costs[j+1] = corner;
+      else {
+        size_t t(upper<corner? upper: corner);
+        costs[j+1] = (costs[j]<t?costs[j]:t)+1;
+      }
+      
+      corner = upper;
+      j++;
+    }
+    i++;
+  }
+  
+  return costs[n];
 }
